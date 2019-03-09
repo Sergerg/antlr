34e8167829bf7dcd9d5a531aa852372b43a516e2
Сергей Галичанин <galichanin@elewise.com>
2019-03-09 15:45:44 +0400

    SQL правила. Разбор простейшего запроса.

--- isimple/src/isimple/sql-parse.clj
+++ isimple/src/isimple/sql-parse.clj
@@ -9,11 +9,34 @@
 (single-a " a   ")
 (insta/visualize (single-a "a") :output-file "images/single-a-1.png" :options {:dpi 63})
 
-;; SQL:
+;; 2
+(def sss
+  "sentence = token (<whitespace> token)*
+    <token> = keyword | identifier
+    whitespace = #'\\s+'
+    identifier = #'[a-zA-Z]+'
+    keyword = 'cond' | 'defn'")
+(println sss)
+(def ambiguous-tokenizer
+  (insta/parser
+   sss))
+(insta/parses ambiguous-tokenizer "defn my cond")
+(insta/visualize
+    (insta/parses ambiguous-tokenizer "defn my cond")
+    :output-file "images/ambiguous-tokenizer-1.png"
+    :options {:dpi 63})
+
+ ;; SQL:
 (def parse-sql
   (insta/parser (io/resource "rules/sql_text.bnf")))
 
-(parse-sql "select")
+(def test-query "select * from table")
+(parse-sql test-query)
+(insta/parses parse-sql test-query)
+(insta/visualize
+ (insta/parses parse-sql test-query)
+ :output-file "images/parse-sql-1.png"
+ :options {:dpi 63})
 
 ;; Каталог TEST
 (io/resource "rules/sql.bnf")
