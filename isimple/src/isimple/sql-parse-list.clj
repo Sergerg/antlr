fadcf16f648bb8d5eaeb453ddcfec839d2f24743
Сергей Галичанин <galichanin@elewise.com>
2019-03-12 20:41:42 +0400

    Обход цикла и создание общей диаграммы.

--- isimple/src/isimple/sql-parse-list.clj
+++ isimple/src/isimple/sql-parse-list.clj
@@ -8,7 +8,17 @@
   (insta/parser (io/resource "rules/sql_text.bnf")
                 :output-format :enlive))
 
-(def queries-list '("select * from table"
+(def queries-list ["select * from table"
                     "select * from table t"
-                    "select t.* from table t"))
+                    "select t.* from table t"])
+queries-list
+(get queries-list 0)
+(parse-sql (get queries-list 0))
+(def res (map parse-sql queries-list))
+res
+(insta/visualize
+ res
+ :output-file "images/parse-sql.png"
+ :options {:dpi 63})
 
+;; TODO: Цикл с созданием диаграм по каждому запросу!
