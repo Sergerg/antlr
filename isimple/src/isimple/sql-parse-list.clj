a1825d317eb1566df9ad3fdc643468bb5d86b9ac
Сергей Галичанин <galichanin@elewise.com>
2019-03-13 10:08:45 +0400

    TODO:

--- isimple/src/isimple/sql-parse-list.clj
+++ isimple/src/isimple/sql-parse-list.clj
@@ -1,7 +1,7 @@
 (ns isimple.core
     (:require [clojure.string :as str])
     (:require [instaparse.core :as insta])
-    (:require [clojure.java.io :as io] ))
+    (:require [clojure.java.io :as io]))
 
 ;; Загрузка правил.
 (def parse-sql
@@ -9,16 +9,22 @@
                 :output-format :enlive))
 
 (def queries-list ["select * from table"
-                    "select * from table t"
-                    "select t.* from table t"])
+                   "select * from table t"
+                   "select t.* from table t"])
 queries-list
 (get queries-list 0)
 (parse-sql (get queries-list 0))
 (def res (map parse-sql queries-list))
 res
+
 (insta/visualize
  res
  :output-file "images/parse-sql.png"
- :options {:dpi 63})
+ :options     {:dpi 63})
 
 ;; TODO: Цикл с созданием диаграм по каждому запросу!
+
+
+;; TODO: Обработка деревьев!
+
+
